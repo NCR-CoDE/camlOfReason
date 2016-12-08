a41e89dabc8b77195a8d4994ff6af06137f65073
johnwatson <john.watson@ncr.com>
2016-12-08 12:14:09 +0000

    initial changes

---
+++ server/src/config.ml
@@ -0,0 +1,38 @@
+open Mirage
+
+let get ~default name =
+  try String.lowercase @@ Sys.getenv name
+  with Not_found -> default
+
+let image = get "XENIMG" ~default:"www"
+
+let http_port =
+  let doc = Key.Arg.info
+      ~doc:"Port to listen on for plain HTTP connections"
+      ~docv:"PORT" ["http-port"]
+  in
+  Key.(create "http-port" Arg.(opt ~stage:`Both int 8080 doc))
+
+let stack = generic_stackv4 default_console tap0
+
+let keys = Key.([ abstract http_port ])
+let http =
+  foreign ~keys "Dispatch.Make"
+    (http @-> kv_ro @-> kv_ro @-> clock @-> job)
+
+let dispatch =
+    (** Without tls *)
+    (http $ http_server (conduit_direct stack))
+
+let libraries = [ "rrd" ]
+let packages  = [ "xapi-rrd" ]
+
+let fs_key = Key.(value @@ kv_ro ())
+let filesfs = generic_kv_ro ~key:fs_key "../files"
+let tmplfs = generic_kv_ro ~key:fs_key "../tmpl"
+
+let () =
+  let tracing = None in
+  (* let tracing = mprof_trace ~size:10000 () in *)
+  register ?tracing ~libraries ~packages image [  dispatch $ filesfs $ tmplfs $ default_clock ]
+(*  register ?tracing ~libraries ~packages image [  dispatch $ filesfs $ tmplfs $ default_clock  ] *)
---
+++ server/src/dispatch.ml
@@ -0,0 +1,48 @@
+open Lwt.Infix
+
+let err fmt = Printf.kprintf (fun f -> raise (Failure f)) fmt
+
+let domain_of_string x =
+  let uri = Uri.of_string x in
+  let scheme = match Uri.scheme uri with
+    | Some "http"  -> `Http
+    | Some "https" -> `Https
+    | _ ->
+      err "%s: wrong scheme for redirect. Should be either http:// or https://." x
+  in
+  let host = match Uri.host uri with
+    | Some x -> x
+    | None   -> err "%s: missing hostname for redirect." x
+  in
+  scheme, host
+
+module Make
+    (S: Cohttp_lwt.Server)
+    (FS: V1_LWT.KV_RO) (TMPL: V1_LWT.KV_RO)
+    (Clock: V1.CLOCK)
+= struct
+
+  let log_src = Logs.Src.create "dispatch" ~doc:"Web server"
+  module Log = (val Logs.src_log log_src : Logs.LOG)
+
+  let respond_ok ?(headers=[]) body =
+    body >>= fun body ->
+    let status = `OK in
+    let headers = Cohttp.Header.of_list headers in
+    S.respond_string ~headers ~status ~body ()
+
+  let create () =
+    let hdr = "HTTP" in
+    let callback (_, conn_id) request _body =
+      let uri = Cohttp.Request.uri request in
+      respond_ok ( Lwt.return "hello world" )
+    in
+    let conn_closed (_,conn_id) =
+      let cid = Cohttp.Connection.to_string conn_id in
+      Log.debug (fun f -> f "[%s %s] OK, closing" hdr cid)
+    in
+    S.make ~callback ~conn_closed ()
+
+  let start http fs tmpl () =
+    http ( `TCP ( 8080 ) ) (create () )
+end
